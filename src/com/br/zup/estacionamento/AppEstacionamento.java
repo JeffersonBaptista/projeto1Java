89e9d1d41251e66a77dd36540112de5ba9c86cd1
jeffersonbaptista <jefferson.baptista@zup.com.br>
2019-06-07 15:54:23 -0300

    criar metodo entrada(Scanner)

--- src/com/br/zup/estacionamento/AppEstacionamento.java
+++ src/com/br/zup/estacionamento/AppEstacionamento.java
@@ -1,10 +1,21 @@
 package com.br.zup.estacionamento;
 
+import java.util.Scanner;
+
 public class AppEstacionamento {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = entrada();
+		
+		int valor = scan.nextInt();
 
+	}
+	
+	public static Scanner entrada() {
+		Scanner scan = new Scanner(System.in);
+		
+		return scan;
 	}
 
 }
